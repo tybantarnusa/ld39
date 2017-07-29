9db579d1a8540c571519236dfe820b47d623bcf0
Mgs. M. Thoyib Antarnusa <tybantarnusa@null.net>
2017-07-29 16:13:47 +0700

    Add interactable and stuffs

---
+++ Assets/InteractScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractScript : MonoBehaviour {
+
+    [SerializeField]
+    private Camera camera;
+
+    [SerializeField]
+    private GameObject indicator;
+
+    void Start()
+    {
+        indicator.SetActive(false);
+    }
+
+    void Update () {
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject hitObject = hit.transform.gameObject;
+            if (hitObject.CompareTag("Charger") || hitObject.CompareTag("Powerbank"))
+            {
+                if(Vector3.Distance(transform.position, hitObject.transform.position) < 2f) { 
+                    indicator.SetActive(true);
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+
+                        if (hitObject.CompareTag("Charger"))
+                        {
+                            GetComponent<PlayerScript>().SetCharger(true);
+                            Destroy(hitObject);
+
+                        }
+                        else if (hitObject.CompareTag("Powerbank"))
+                        {
+                            GetComponent<PlayerScript>().SetPowerbank(true);
+                            Destroy(hitObject);
+                        }
+                    }
+                }
+            } else
+            {
+                indicator.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    private bool hasCharger;
+    private bool hasPowerbank;
+
+    private bool usingFlashlight;
+
+    private float battery;
+    private float timer;
+
+    [SerializeField]
+    private GameObject flashlight;
+
+	void Start () {
+        hasCharger = false;
+        hasPowerbank = false;
+        usingFlashlight = true;
+
+        battery = 100f;
+
+        timer = 0f;
+	}
+	
+	void Update () {
+        HandleInput();
+        //Debug.Log("Charger: " + hasCharger + " | Powerbank: " + hasPowerbank);
+
+        timer += Time.deltaTime;
+	    if (usingFlashlight)
+        {
+
+        }	
+	}
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && usingFlashlight)
+        {
+            flashlight.GetComponent<Light>().intensity = 0.3f;
+            usingFlashlight = false;
+        } else if (Input.GetKeyDown(KeyCode.F) && !usingFlashlight)
+        {
+            flashlight.GetComponent<Light>().intensity = 1.8f;
+            usingFlashlight = true;
+        }
+    }
+
+    public void SetCharger(bool yes)
+    {
+        hasCharger = yes;
+    }
+
+    public void SetPowerbank(bool yes)
+    {
+        hasPowerbank = yes;
+    }
+}
